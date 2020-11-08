13089217f5163bd4bc00c2edf24639785c9f2269
panmengjia <2926314245@qq.com>
2020-11-08 16:01:00 +0800

    将类成员函数作为多线程回调函数

--- gitonVS/main.cpp
+++ gitonVS/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <opencv.hpp>
+#include <thread>
+#include "windows.h"
+
+using namespace cv;
+using namespace std;
+
+class threadtest
+{
+public:
+	threadtest() {};
+	threadtest(int& a, int& b, int& c);
+
+	friend void threadCallback(threadtest* threadtestPtr);
+
+	int a;
+	int b;
+private:
+	int c = 100;
+};
+
+void threadCallback(threadtest* threadtestPtr)
+{
+
+	while (1)
+	{
+		puts("另外一个线程");
+		Sleep(500);
+		threadtestPtr->b++;
+		cout << "threadtestPtr->b = " << threadtestPtr->b << endl;
+	}
+}
+
+threadtest::threadtest(int& a, int& b,int& c)
+{
+	this->a = a;
+	this->b = b;
+	this->c = c;
+}
+
+int gittest();
 
 int main()
+{
+	int a = 10;
+	int b = 57;
+	int c = 98;
+	threadtest* ptr = new threadtest(a,b,c);
+	thread another(threadCallback, ptr);
+	another.detach();
+
+	while (1)
+	{
+		puts("主线程------");
+		Sleep(1000);
+		gittest();
+	}
+
+	return 0;
+}
+
+int gittest()
 {
 	int* p;
 	int a[10];
@@ -10,7 +70,7 @@ int main()
 	}
 	p = a;
 	printf("%d,%d\n", *p, *a);
-	printf("%x", p + 1);
+	printf("%p\n", (p + 1)); //把%x换成%p
 
 	return 0;
 
